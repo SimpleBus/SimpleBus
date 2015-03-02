76cbdb443b005423f1d2937bbf940dbcbcc7798c
Matthias Noback <matthiasnoback@gmail.com>
2015-03-02 14:29:18 +0100

    Simply JMS object serializer

--- tests/Integration/SampleMessage.php
+++ tests/Integration/SampleMessage.php
@@ -3,23 +3,7 @@
 namespace SimpleBus\JMSSerializerBridge\Tests\Integration;
 
 use SimpleBus\Message\Message;
-use JMS\Serializer\Annotation as Serializer;
 
 class SampleMessage implements Message
 {
-    /**
-     * @Serializer\Type("string")
-     */
-    private $foo;
-
-    /**
-     * @Serializer\Type("integer")
-     */
-    private $bar;
-
-    public function __construct($foo, $bar)
-    {
-        $this->foo = $foo;
-        $this->bar = $bar;
-    }
 }
