a63bdeb5a98e188cceef62b637b04b51c0d685d5
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-03 12:11:42 +0100

    Added spellcheck

--- conf.py
+++ conf.py
@@ -29,7 +29,13 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinxcontrib.spelling'
+]
+
+# Spelling configuration
+spelling_lang='en_US'
+spelling_word_list_filename='spelling_word_list.txt'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
