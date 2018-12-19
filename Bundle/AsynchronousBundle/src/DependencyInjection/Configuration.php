c15aa6629f8863f0633488110034958c4c9e3b19
Oleksandr Besedin <bs@cosmonova.net>
2018-12-19 16:23:29 +0200

    removed deprecation notices on symfony >=4.2

--- Bundle/AsynchronousBundle/src/DependencyInjection/Configuration.php
+++ Bundle/AsynchronousBundle/src/DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder($this->alias);
         $rootNode = $treeBuilder->root($this->alias);
 
         $rootNode
