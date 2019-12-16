ad8665d823fe9709cd8e8532a73247a3a33488c7
Joakim Friberg <joakim.friberg@gmail.com>
2019-12-16 15:12:53 +0100

    Preserve the priority when sorting compiler passes in new versions of symfony

--- Bundle/SymfonyBridge/src/DependencyInjection/Compiler/CompilerPassUtil.php
+++ Bundle/SymfonyBridge/src/DependencyInjection/Compiler/CompilerPassUtil.php
@@ -4,6 +4,7 @@ namespace SimpleBus\SymfonyBridge\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
 class CompilerPassUtil
 {
@@ -11,9 +12,14 @@ class CompilerPassUtil
         ContainerBuilder $container,
         CompilerPassInterface $compilerPass
     ) {
-        $compilerPassConfig = $container->getCompilerPassConfig();
-        $beforeOptimizationPasses = $compilerPassConfig->getBeforeOptimizationPasses();
-        array_unshift($beforeOptimizationPasses, $compilerPass);
-        $compilerPassConfig->setBeforeOptimizationPasses($beforeOptimizationPasses);
+        $rc = new \ReflectionMethod(PassConfig::class, 'addPass');
+        if ($rc->getNumberOfParameters() >= 3 || method_exists(PassConfig::class, 'sortPasses')) {
+            $container->addCompilerPass($compilerPass, PassConfig::TYPE_BEFORE_OPTIMIZATION, 50);
+        } else {
+            $compilerPassConfig = $container->getCompilerPassConfig();
+            $beforeOptimizationPasses = $compilerPassConfig->getBeforeOptimizationPasses();
+            array_unshift($beforeOptimizationPasses, $compilerPass);
+            $compilerPassConfig->setBeforeOptimizationPasses($beforeOptimizationPasses);
+        }
     }
 }
