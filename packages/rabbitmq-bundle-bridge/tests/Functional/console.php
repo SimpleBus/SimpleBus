59a46b02616e0de289470ea9d3ebc5221d9f789f
Cliff Odijk <cliff@jcid.nl>
2020-12-09 18:31:21 +0100

    Fix the tests for RabbitMQ

--- packages/rabbitmq-bundle-bridge/tests/Functional/console.php
+++ packages/rabbitmq-bundle-bridge/tests/Functional/console.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../bootstrap.php';
 use SimpleBus\RabbitMQBundleBridge\Tests\Functional\TestKernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 
 $input = new ArgvInput();
 
