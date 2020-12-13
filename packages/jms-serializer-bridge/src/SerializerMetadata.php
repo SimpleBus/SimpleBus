77f2b6d7e0088a9ebddd76182e4393e790476067
Cliff Odijk <cliff@jcid.nl>
2020-12-13 12:31:13 +0100

    Make the code strict for `SimpleBus\JMSSerializerBridge`

--- packages/jms-serializer-bridge/src/SerializerMetadata.php
+++ packages/jms-serializer-bridge/src/SerializerMetadata.php
@@ -4,12 +4,12 @@ namespace SimpleBus\JMSSerializerBridge;
 
 final class SerializerMetadata
 {
-    public static function directory()
+    public static function directory(): string
     {
         return __DIR__.'/metadata';
     }
 
-    public static function namespacePrefix()
+    public static function namespacePrefix(): string
     {
         return 'SimpleBus\Serialization';
     }
